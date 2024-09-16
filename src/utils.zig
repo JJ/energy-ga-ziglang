814852532de6e686f117bee32810f5e239ad0eb4
JJ Merelo <jjmerelo@gmail.com>
2024-09-16 12:28:15 +0200

    #7 start spinning off random seed

--- src/utils.zig
+++ src/utils.zig
@@ -3,14 +3,22 @@ const expect = std.testing.expect;
 
 // Random number generator used all over
 pub fn ourRng() !std.Random.DefaultPrng {
-    return std.Random.DefaultPrng.init(blk: {
-        var seed: u64 = undefined;
-        try std.posix.getrandom(std.mem.asBytes(&seed));
-        break :blk seed;
-    });
+    return std.Random.DefaultPrng.init(try createSeed());
 }
 
-// test the generator
+pub fn createSeed() !u64 {
+    var seed: u64 = undefined;
+    try std.posix.getrandom(std.mem.asBytes(&seed));
+    return seed;
+}
+
+test "Random seed" {
+    const seed = try createSeed();
+    try expect(seed != 0);
+    const anotherSeed = try createSeed();
+    try expect(seed != anotherSeed);
+}
+
 test "random generators" {
     var first_rng = try ourRng();
     for (1..10) |_| {
