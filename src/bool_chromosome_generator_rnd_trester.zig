28ee6583da5c1fb4673d3f00fe9dcacca8fc5238
JJ Merelo <jjmerelo@gmail.com>
2024-09-16 13:11:19 +0200

    :stop_sign: impasse since this can't be done

--- src/bool_chromosome_generator_rnd_trester.zig
+++ src/bool_chromosome_generator_rnd_trester.zig
@@ -1,21 +1,37 @@
 const std = @import("std");
 const boolGenerate = @import("bool_generate.zig").boolGenerate;
+const createSeed = @import("utils.zig").createSeed;
 const ourRng = @import("utils.zig").ourRng;
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    var prng: std.rand.DefaultPrng = try ourRng();
-    const rndGen = prng.random();
+
     var argsIterator = try std.process.argsWithAllocator(allocator);
     defer argsIterator.deinit();
 
     _ = argsIterator.next(); // First argument is the program name
 
-    const stringLenArg = argsIterator.next() orelse "512";
+    const rngArgStr = argsIterator.next() orelse "0";
+    const rngArg = try std.fmt.parseInt(u16, rngArgStr, 10);
 
-    const stringLength = try std.fmt.parseInt(u16, stringLenArg, 10);
+    var prng: std.Random = undefined;
+    const thisSeed: u64 = try createSeed();
+    switch (rngArg) {
+        0 => {
+            prng = std.Random.Isaac64.init(thisSeed);
+        },
+        1 => {
+            prng = std.Random.Pcg.init(thisSeed);
+        },
+        else => {
+            std.debug.print("Invalid argument: {}\n", .{rngArg});
+            return;
+        },
+    }
+    const rndGen = prng.random();
 
-    const numStrings = 40000;
+    const numStrings = 65535;
+    const stringLength = 1024;
 
     const output = try boolGenerate(allocator, rndGen, stringLength, numStrings);
     std.debug.print("Generated {} strings of length {}\n", .{ numStrings, stringLength });
