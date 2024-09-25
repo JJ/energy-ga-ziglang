edccefcbcb8c269263263a8c96c9a44711087df9
JJ Merelo <jjmerelo@gmail.com>
2024-09-25 10:42:23 +0200

    #11 :white_check_mark: :recycle:

--- src/my_library.zig
+++ src/my_library.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
-pub const generate = @import("generate.zig").generate;
+pub const generate = @import("string_generate.zig").generate;
 pub const boolGenerate = @import("bool_generate.zig").boolGenerate;
-pub const crossover = @import("crossover.zig").crossover;
-pub const mutation = @import("mutation.zig").mutation;
+pub const crossover = @import("string_crossover.zig").crossover;
+pub const mutation = @import("string_mutation.zig").mutation;
 pub const countOnes = @import("count_ones.zig").countOnes;
 pub const HIFF = @import("HIFF.zig").HIFF;
 
